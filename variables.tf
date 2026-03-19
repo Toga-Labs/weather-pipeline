7823e34b1528fe27537f7ee334f66163d14a04c9
toga <togamkandawire@gmail.com>
2026-03-19 18:59:17 +0000

    fix: added variable root tf Flag to toggle infrastructure modules on or off

--- variables.tf
+++ variables.tf
@@ -1,23 +1,36 @@
 variable "project_name" {
-  type = string
+  type        = string
+  description = "Base name used to prefix all AWS resources created by this project."
 }
 
 variable "region" {
-  type = string
+  type        = string
+  description = "AWS region where all resources will be deployed."
 }
 
 variable "city" {
-  type = string
+  type        = string
+  description = "City name used for weather API requests and naming conventions."
 }
 
 variable "weather_api_key" {
-  type = string
+  type        = string
+  description = "API key used to authenticate requests to the external weather data provider."
 }
 
 variable "raw_prefix" {
-  type = string
+  type        = string
+  description = "S3 prefix (folder path) where raw ingested weather data will be stored."
 }
 
 variable "scripts_bucket" {
-  type = string
+  type        = string
+  description = "Name of the S3 bucket that stores Lambda code and ETL scripts.Flag to toggle infrastructure modules on or off"
+}
+
+
+variable "enable_infra" {
+  description = "Controls whether infra modules (IAM, S3, Glue) are deployed"
+  type        = bool
+  default     = true
 }
