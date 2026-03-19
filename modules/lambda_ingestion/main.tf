36e00b2a4d216946753f44062cc397cb0c5db381
toga <togamkandawire@gmail.com>
2026-03-19 19:54:52 +0000

    fix: remove CloudWatch log group resource to prevent ResourceAlreadyExistsException

--- modules/lambda_ingestion/main.tf
+++ modules/lambda_ingestion/main.tf
@@ -81,11 +81,3 @@ resource "aws_lambda_function" "ingestion" {
   }
 }
 
-###############################################
-# CLOUDWATCH LOG GROUP
-###############################################
-
-resource "aws_cloudwatch_log_group" "lambda_logs" {
-  name              = "/aws/lambda/${aws_lambda_function.ingestion.function_name}"
-  retention_in_days = 14
-}
